f1d54ca2556039979d345a7311562c115afc869a
simon <tran.simon2@gmail.com>
2021-02-15 15:43:26 -0500

    added gitignore

--- examples/test/bin/dep/ffunicode.o.d
+++
@@ -1,9 +0,0 @@
-ffunicode.o: ../../src/lib/FatFS/ffunicode.c ../../src/lib/FatFS/ff.h \
- ../../src/config/ffconf.h \
- /usr/lib/gcc/arm-none-eabi/9.2.1/include/stdint.h
-
-../../src/lib/FatFS/ff.h:
-
-../../src/config/ffconf.h:
-
-/usr/lib/gcc/arm-none-eabi/9.2.1/include/stdint.h:
