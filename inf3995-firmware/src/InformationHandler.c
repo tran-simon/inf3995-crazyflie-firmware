3066448e866cb9678daf0d30eb866ad891b9c63b
Jacob Brisson <jacob.brisson@polymtl.ca>
2021-02-22 16:39:30 -0500

    INF3995 #2 - Getiing the state of drones

--- inf3995-firmware/src/InformationHandler.c
+++ inf3995-firmware/src/InformationHandler.c
@@ -1,9 +1,17 @@
 #include "../interface/InformationHandler.h"
 
+
 struct Information getStats(char command){
     struct Information response;
     setpoint_t setpoint;
     state_t state;
+    float speed = 0.0f;
+
+    crtpCommanderHighLevelGetSetpoint(&setpoint, &state);
+    if(!isnanf(state.velocity.x) && !isnanf(state.velocity.y) && !isnanf(state.velocity.z)) {
+        speed = sqrtf(powf(state.velocity.x, 2) + powf(state.velocity.y, 2) + powf(state.velocity.z, 2));
+    }
+    
 
     switch (command)
     {
@@ -13,15 +21,20 @@ struct Information getStats(char command){
         break;
     
     case 'v':
-        crtpCommanderHighLevelGetSetpoint(&setpoint, &state);
-        if(!isnanf(state.velocity.x) && !isnanf(state.velocity.y) && !isnanf(state.velocity.z)) {
-            response.value = sqrtf(powf(state.velocity.x, 2) + powf(state.velocity.y, 2) + powf(state.velocity.z, 2));
-        }
-        else{
-            response.value = 0.0f;
-        }
+        response.value = speed;
         response.type = 'v';
         break;
+
+    case 's':
+        if(speed > 0.0f) {
+            response.value = 1.0f;
+        }
+        else {
+            response.value = 0.0f;
+        }
+        response.type = 's';
+        break;
+
 
     default:
         break;
