2fb057ca81cbe5f9345b1089c477e1a9981fc145
Jacob <jacbri1998@gmail.com>
2021-04-17 12:51:01 -0700

    Added tests for CommandHandler

--- run_test.sh
+++ run_test.sh
@@ -2,4 +2,4 @@ sudo docker kill firm
 sudo docker rm firm
 sudo docker build . -t firmware
 sudo docker run -d --name firm firmware
-sudo docker exec firm /bin/sh -c "cd ..;cd inf3995-firmware;make unit FILES=test/project/test_SensorCommands.c;make unit FILES=test/project/test_InformationCommands.c"
+sudo docker exec firm /bin/sh -c "cd ..;cd inf3995-firmware;make unit FILES=test/project/test_SensorCommands.c;make unit FILES=test/project/test_InformationHandler.c;make unit FILES=test/project/test_CommandHandler.c"
---
+++ test/testSupport/CommandHandlerMock.c
@@ -0,0 +1,59 @@
+#include "CommandHandlerMock.h"
+
+struct Information activateCommand(char command, bool returnToBase){
+    struct Information response;
+
+    switch (command)
+    {
+    //Take off
+    case 't':
+        //takeOff();
+        response.type = 't';
+        response.value1 = 1.0f;
+        response.value2 = 0.0f;
+        response.value3 = 0.0f;
+        response.value4 = 0.0f;
+        response.value5 = 0.0f;
+        response.value6 = 0.0f;
+        break;
+    //Explore
+    case 'e':
+        //exploration();
+        response.type = 't';
+        response.value1 = 1.0f;
+        response.value2 = 0.0f;
+        response.value3 = 0.0f;
+        response.value4 = 0.0f;
+        response.value5 = 0.0f;
+        response.value6 = 0.0f;
+        break;
+    // Land
+    case 'l':
+        //land();
+        response.type = 'l';
+        response.value1 = 1.0f;
+        response.value2 = 0.0f;
+        response.value3 = 0.0f;
+        response.value4 = 0.0f;
+        response.value5 = 0.0f;
+        response.value6 = 0.0f;
+        break;
+
+    // Return to base
+    case 'r':
+        response.value1 = returnToBase ? 1 : 0;
+        response.value2 = 0.0f;
+        response.value3 = 0.0f;
+        response.value4 = 0.0f;
+        response.value5 = 0.0f;
+        response.value6 = 0.0f;
+        response.type = 'r';
+        break;
+
+    default:
+        break;
+    };
+
+    return response;
+
+}
