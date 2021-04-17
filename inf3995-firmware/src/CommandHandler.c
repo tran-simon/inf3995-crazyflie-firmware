d7e2fef93aa5f67e23ec6176bd5a5afa2c12b0d3
Matthew <matthew-justin.paoli@polymtl.ca>
2021-04-17 15:58:47 -0400

    Defined values 2-6 in commandHandler

--- inf3995-firmware/src/CommandHandler.c
+++ inf3995-firmware/src/CommandHandler.c
@@ -9,25 +9,45 @@ struct Information activateCommand(char command){
     case 't':
         takeOff();
         response.type = 't';
-        response.value1 = 1;
+        response.value1 = 1.0f;
+        response.value2 = 0.0f;
+        response.value3 = 0.0f;
+        response.value4 = 0.0f;
+        response.value5 = 0.0f;
+        response.value6 = 0.0f;
         break;
     //Explore
     case 'e':
         exploration();
         response.type = 't';
-        response.value1 = 1;
+        response.value1 = 1.0f;
+        response.value2 = 0.0f;
+        response.value3 = 0.0f;
+        response.value4 = 0.0f;
+        response.value5 = 0.0f;
+        response.value6 = 0.0f;
         break;
     // Land
     case 'l':
         land();
         response.type = 'l';
-        response.value1 = 0;
+        response.value1 = 0.0f;
+        response.value2 = 0.0f;
+        response.value3 = 0.0f;
+        response.value4 = 0.0f;
+        response.value5 = 0.0f;
+        response.value6 = 0.0f;
         break;
 
     // Return to base
     case 'r':
-        response.value1 = returnToBase() ? 1 : 0;
         response.type = 'r';
+        response.value1 = returnToBase() ? 1.0f : 0.0f;
+        response.value2 = 0.0f;
+        response.value3 = 0.0f;
+        response.value4 = 0.0f;
+        response.value5 = 0.0f;
+        response.value6 = 0.0f;
         break;
 
     default:
