88c587a0301d2162368b9218ba3054a8c8fc9c72
Jacob <jacbri1998@gmail.com>
2021-04-17 11:11:57 -0700

    Fix test script

--- run_test.sh
+++ run_test.sh
@@ -1,2 +1,5 @@
-make unit FILES=test/project/test_SensorCommands.c
-make unit FILES=test/project/test_InformationCommands.c
+sudo docker kill firm
+sudo docker rm firm
+sudo docker build . -t firmware
+sudo docker run -d --name firm firmware
+sudo docker exec firm /bin/sh -c "cd ..;cd inf3995-firmware;make unit FILES=test/project/test_SensorCommands.c;make unit FILES=test/project/test_InformationCommands.c"
